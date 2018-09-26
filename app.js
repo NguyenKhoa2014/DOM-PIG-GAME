1eddd99c3d470e86f070b5f185bae941eb491116
NguyenKhoa2014 <nguyenkhoa@gmail.com>
2018-09-26 07:35:13 -0700

    add logic for rolling dice and keeping track of active player score

--- app.js
+++ app.js
@@ -13,12 +13,14 @@ var scores, roundScore, activePlayer;
 scores = [0,0];
 roundScore = 0;
 activePlayer = 0;
-
- 
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0'; 
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
 
 //document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
 
-var x = document.querySelector('#score-0').textContent;
+//var x = document.querySelector('#score-0').textContent;
 
 document.querySelector('.dice').style.display = 'none'; 
 
@@ -28,5 +30,26 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
     var diceDOM = document.querySelector('.dice');
     diceDOM.style.display = 'block';
     diceDOM.src = 'dice-' + number + '.png';
+    
+    if (number !== 1){
+        roundScore += number;
+        document.querySelector('#current-' + activePlayer).textContent = roundScore;
+    } else {
+        activePlayer === 0 ? activePlayer = 1: activePlayer = 0;
+        roundScore = 0;
+
+        document.getElementById('current-0').textContent = '0';
+        document.getElementById('current-1').textContent = '0';
+
+        document.querySelector('.player-0-panel').classList.toggle('active');
+        document.querySelector('.player-1-panel').classList.toggle('active');
+        // document.querySelector('.player-0-panel').classList.remove('active');
+        // document.querySelector('.player-1-panel').classList.add('active');
+        document.querySelector('.dice').style.display = none;
+    }
 
 });
+
+document.querySelector('.btn-hold').addEventListener('click', function() {
+    //
+})
