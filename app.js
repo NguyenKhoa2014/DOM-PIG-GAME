935519226dba5ee8c88f578136350349052a16d4
NguyenKhoa2014 <nguyenkhoa@gmail.com>
2018-09-27 20:40:05 -0700

    implement btn new and btn hold logic

--- app.js
+++ app.js
@@ -9,14 +9,8 @@ GAME RULES:
 
 */
 var scores, roundScore, activePlayer;
-
-scores = [0,0];
-roundScore = 0;
-activePlayer = 0;
-document.getElementById('score-0').textContent = '0';
-document.getElementById('score-1').textContent = '0'; 
-document.getElementById('current-0').textContent = '0';
-document.getElementById('current-1').textContent = '0';
+init();
+ 
  
 
 document.querySelector('.dice').style.display = 'none'; 
@@ -37,12 +31,7 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
 
 });
 
-document.querySelector('.btn-hold').addEventListener('click', function() {
-    scores[activePlayer] += roundScore;
-    
-    document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
-    nextPlayer();
-});
+
 
 function nextPlayer() {
     activePlayer === 0 ? activePlayer = 1: activePlayer = 0;
@@ -53,16 +42,38 @@ function nextPlayer() {
 
     document.querySelector('.player-0-panel').classList.toggle('active');
     document.querySelector('.player-1-panel').classList.toggle('active');
-    // document.querySelector('.player-0-panel').classList.remove('active');
-    // document.querySelector('.player-1-panel').classList.add('active');
-    document.querySelector('.dice').style.display = none;
+    
+    document.querySelector('.dice').style.display = 'none';
 }
 
 document.querySelector('.btn-hold').addEventListener('click', function() {
     // Add current score to global Score
     scores[activePlayer] += roundScore;
     // Update the UI
-    document.querySelector('score-' + activePlayer).textContent = scores[activePlayer];
+    document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
     // Check if the player won the game
+    if (scores[activePlayer] >= 20) {
+        document.querySelector('#name-' + activePlayer).textContent = 'Winner';
+        document.querySelector('.dice').style.display = 'none';
+        document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+        document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+    } else {
+        nextPlayer();
+    }
+});
 
-})
+document.querySelector('.btn-new').addEventListener('click', init);
+
+function init() {
+    scores = [0,0];
+    roundScore = 0;
+    activePlayer = 0;
+    document.querySelector('.dice').style.display = 'none'; //hide the card
+    document.getElementById('score-0').textContent = '0';
+    document.getElementById('score-1').textContent = '0'; 
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+
+    document.querySelector('#name-1' ).textContent = 'Player 2';
+    document.querySelector('#name-0' ).textContent = 'Player 1';
+}
